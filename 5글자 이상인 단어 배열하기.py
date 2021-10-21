f23068147ca21e553dd1ef38e95d226a9a8a2b6f
효정이 <w2029@e-mirim.hs.kr>
2021-10-22 00:07:40 +0900

    5글자 이상인 단어 배열하기 (다시해보기)

---
+++ 5글자 이상인 단어 배열하기.py
@@ -0,0 +1,15 @@
+def solution(words):
+	answer = ''
+	for w in words:
+		if len(w) >= 5:
+			answer += w
+	if len(answer) < 1:
+		answer = "empty"
+	return answer
+words1 = ["my", "favorite", "color", "is", "violet"]
+ret1 = solution(words1);
+
+print("solution 함수의 반환 값은", ret1, "입니다.")
+
+words2 = ["yes", "i", "am"]
+ret2 = solution(words2);
