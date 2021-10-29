a3ba2789b8eba36d985cec92520bda72ab3b88be
효정이 <w2029@e-mirim.hs.kr>
2021-10-29 15:33:12 +0900

    내 절반이 여기 있는가 완성

---
+++ 내 절반이 여기 있는가.py
@@ -0,0 +1,11 @@
+def solution(arr):
+	answer = 0
+	for i in arr:
+		if (i/2) in arr:
+			answer += 1
+	return answer
+arr = [4, 8, 3, 6, 7]
+ret = solution(arr)
+
+#[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+print("solution 함수의 반환 값은", ret, "입니다.")
