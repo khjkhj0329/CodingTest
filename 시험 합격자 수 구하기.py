9d7f541beaea5964f57bee3960d3b549b7a30199
효정이 <w2029@e-mirim.hs.kr>
2021-10-31 22:02:40 +0900

    시험 합격자 수 구하기 완성

---
+++ 시험 합격자 수 구하기.py
@@ -0,0 +1,11 @@
+def solution(scores, cutline):
+	answer = 0
+	for s in scores:
+		if s >= cutline:
+			answer += 1
+	return answer
+scores = [80, 90, 55, 60, 59]
+cutline = 60
+ret = solution(scores, cutline)
+
+print("solution 함수의 반환 값은", ret, "입니다.")
