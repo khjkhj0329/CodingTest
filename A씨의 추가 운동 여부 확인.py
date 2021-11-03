5a1ab8c31ecaea959c71042d2964df3f6a1851a2
효정이 <w2029@e-mirim.hs.kr>
2021-11-03 19:04:56 +0900

    A씨의 추가 운동 여부 확인 완성

---
+++ A씨의 추가 운동 여부 확인.py
@@ -0,0 +1,13 @@
+def solution(calorie):
+	min_cal = 1001
+	answer = 0
+	for cal in calorie:
+		if cal > min_cal:
+			answer += cal - min_cal
+		min_cal = min(min_cal, cal)
+	return answer
+calorie = [713, 665, 873, 500, 751]
+ret = solution(calorie)
+
+# [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+print("solution 함수의 반환 값은", ret, "입니다.")
